2f7bdb290076756ccaadee2b8678f630ceb3ec65
Korwex Hajaco <damik1318@gmail.com>
2020-08-12 05:24:15 +0200

    Updates

---
+++ precache-manifest.d9b3d83c443b541a0dc3dba09deef8e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6b16a2e6f36131af5dac0a6c0155368",
+    "url": "/rybex/index.html"
+  },
+  {
+    "revision": "117409429e48d1ecc99e",
+    "url": "/rybex/static/css/main.738c648b.chunk.css"
+  },
+  {
+    "revision": "f92d9cefddd339df80cb",
+    "url": "/rybex/static/js/2.c378e63f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rybex/static/js/2.c378e63f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "117409429e48d1ecc99e",
+    "url": "/rybex/static/js/main.a9317e9d.chunk.js"
+  },
+  {
+    "revision": "151a243de8a389b911da",
+    "url": "/rybex/static/js/runtime-main.39f6fd8f.js"
+  },
+  {
+    "revision": "bbbd0bdbfba093dec2ee4ca8b62e61f9",
+    "url": "/rybex/static/media/LiberationSans.bbbd0bdb.ttf"
+  }
+]);
