767a09a6795456d36e52b8613fe1c10d38cea230
Korwex Hajaco <damik1318@gmail.com>
2020-08-13 16:28:51 +0200

    first commit

---
+++ precache-manifest.18875558894a935dda08d573b6969cf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d0272dbff9fff71fed5004ba78b5995",
+    "url": "/rybex/index.html"
+  },
+  {
+    "revision": "2d333a65e4b23003540c",
+    "url": "/rybex/static/css/main.aeda6332.chunk.css"
+  },
+  {
+    "revision": "f92d9cefddd339df80cb",
+    "url": "/rybex/static/js/2.c378e63f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rybex/static/js/2.c378e63f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d333a65e4b23003540c",
+    "url": "/rybex/static/js/main.8108a2bf.chunk.js"
+  },
+  {
+    "revision": "151a243de8a389b911da",
+    "url": "/rybex/static/js/runtime-main.39f6fd8f.js"
+  },
+  {
+    "revision": "bbbd0bdbfba093dec2ee4ca8b62e61f9",
+    "url": "/rybex/static/media/LiberationSans.bbbd0bdb.ttf"
+  }
+]);
